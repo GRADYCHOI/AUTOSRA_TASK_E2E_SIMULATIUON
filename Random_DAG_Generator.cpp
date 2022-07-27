9b89a5367594e6e3b5c775f96b1f5a2929d3721a
chldls12 <chldls12@kookmin.ac.kr>
2022-07-27 15:43:55 +0900

    making Random generator...

--- Random_DAG_Generator.cpp
+++ Random_DAG_Generator.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 #include<stdlib.h>
+#include<random>
+#include<time.h>
+#include<cstdlib>
 using namespace std;
+
+int Decide_Input_run(int INPUT[], int n);
 
 class DAG //러너블 개수 = n, 러너블 목록[n], 엣지목록[n][n-1], 입력러너블[], 출력러너블[]
 { 
@@ -9,16 +14,20 @@ private:
 public:
     int num;
     int Input() {
-        cout << "Please Enter the number of Runnable Node : ";
+        cout << "Please Enter the number of Runnable Node(50 ~ 500) : ";
         cin >> num;
         int runnable[num] = {0};
+        cout << "Runnable number : " << num << endl;
         int edge[num][num-1] = {0, };
-        int Input_run[num] = {0};
+        //int Input_run[num] = {0};
+        int Input_num = Decide_Input_run(runnable, num);
+        int Output_num = Decide_Output_run(runnable, num, Input_num);
         int Output_run[num] = {0}; 
+        
         for (int i = 0; i < num; i++) {
-            for (int j = 0; j < num-1; j++) printf("%d ", runnable[i]);
-            printf("\n");
+            printf("%d ", runnable[i]);
         }
+        
         return num;
     } 
     //void GetEdge();
@@ -35,6 +44,19 @@ public:
 
 };
 
+int Decide_Input_run(int INPUT[], int n) {
+    int Input_count = rand() % 5;
+    for (int i = 0; i <= Input_count; i++) INPUT[i] = i+1;
+
+    return Input_count;
+}
+
+int Decide_Output_run(int INPUT[], int n, int input) {
+    int output_count = rand() % 5;
+    for (int i = n; i >= Output_count; i++) INPUT[i] = i+1;
+
+    return Input_count;
+}
 /*
 void Runnable_Generator() {
     int run_num = random 1~100;
@@ -56,8 +78,10 @@ void Random_DAG_Generate(~~~,~~~,~~~) {
 }
 */
 int main() {
+    srand(time(NULL));
     DAG DAG1;
     DAG1.Input();
     //DAG_Generator();
-    cout << DAG1.num << endl;
+
+    return 0;
 }
