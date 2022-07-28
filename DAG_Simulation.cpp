3941c1efe709898d8912e209db17e01850866462
kimhongsuk <ipeach0424@naver.com>
2022-07-28 16:26:49 +0900

    make frame

--- DAG_Simulation.cpp
+++ DAG_Simulation.cpp
@@ -1,7 +1,17 @@
-#include<iostream>
-#include<stdlib.h>
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
 
-int main() {
 
+void set_release_table(DAG dag, vector<vector<double>> release_table);
+void set_execution_table(DAG dag, vector<vector<double>> release_table, vector<vector<pair<double, double>>> execution_table);
+void get_reaction_time(DAG dag, vector<vector<pair<double, double>>> execution_table);
+void get_data_age(DAG dag, vector<vector<pair<double, double>>> execution_table);
+void get_worst_reaction_time();
+void get_worst_data_age();
+
+int main(int argc, char **argv) {
+    vector<vector<double>> release_table;
+    vector<vector<pair<double, double>>> execution_table;
 
 }
