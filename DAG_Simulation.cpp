6701efe68a5bd900e7a7674958a3c95b610fafd8
kimhongsuk <ipeach0424@naver.com>
2022-08-01 15:06:38 +0900

    Checkpoint 1

--- DAG_Simulation.cpp
+++ DAG_Simulation.cpp
@@ -3,8 +3,10 @@
 #include <vector>
 
 
-void set_release_table(DAG dag, vector<vector<double>> release_table);
+void set_release_table(DAG& dag, vector<vector<double>> release_table);
 void set_execution_table(DAG dag, vector<vector<double>> release_table, vector<vector<pair<double, double>>> execution_table);
+void set_hyperperiod(DAG dag);
+void get_release_time(double period, double offset, double hyperperiod, vector<double> *release_table_1task);
 void get_reaction_time(DAG dag, vector<vector<pair<double, double>>> execution_table);
 void get_data_age(DAG dag, vector<vector<pair<double, double>>> execution_table);
 void get_worst_reaction_time();
@@ -13,5 +15,32 @@ void get_worst_data_age();
 int main(int argc, char **argv) {
     vector<vector<double>> release_table;
     vector<vector<pair<double, double>>> execution_table;
+
+}
+
+void set_release_table(DAG& dag, vector<vector<double>> *release_table) {
+
+    for (int task = 0; task < dag.hyperperiod; task++) {
+        int period = dag.runnable[task].period;
+        int offset = dag.runnable[task].offset;
+        vector<double> release_times;
+
+        for (int cycle = 0; cycle < dag.runnable[task].cycle; cycle++) {
+            release_times.push_back(period * cycle + offset);
+        }
+
+        release_table->push_back(release_times);
+        release_times.clear();
+    }
+
+}
+
+void set_execution_table(DAG& dag, vector<vector<double> *release_table, vector<vector<pair<double, double>>> execution_table) {
+    vector<pair<double,double>> empty_time;
+    empty_time.push_back({0, dag.hypertime});
+
+    for (int task = 0; task < dag.hyperperiod; task++) {
+        int 
+    }
 
 }
